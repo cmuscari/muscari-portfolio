b7c1f758938a2e7238c9e1d0157a040c178c57bd
Cortnie <cortniemuscari@gmail.com>
2022-06-22 15:40:15 -0500

    updated projects array

--- muscari-portfolio/src/components/Portfolio/index.js
+++ muscari-portfolio/src/components/Portfolio/index.js
@@ -4,19 +4,39 @@ import Project from '../Project';
 function Portfolio() {
     const projectArray = [
         {
-            image: "./project-images/10.jpg",
+            image: "./project-images/13.jpg",
             name: "Food Junkies",
-            description: "lorem"
+            description: "A restaurant recommendation site that was created using Node.js, Express.js, & MySQL",
+            appLink: "https://murmuring-crag-27720.herokuapp.com/",
+            githubLink: "https://github.com/cmuscari/food-junkies"
+        },
+        {
+            image: "./project-images/10.jpg",
+            name: "COVID-19 Risk Tracker",
+            description: "A travel information site that was created using server-side API's",
+            appLink: "https://sabotagecat.github.io/covid-risk-tracking-site/",
+            githubLink: "https://github.com/cmuscari/covid-risk-tracking-site"
         },
         {
             image: "./project-images/11.jpg",
-            name: "Food Junkies2",
-            description: "lorem"
+            name: "Work Day Scheduler",
+            description: "A scheduling site that was created utilizing local storage",
+            appLink: "https://cmuscari.github.io/super-disco/",
+            githubLink: "https://github.com/cmuscari/super-disco"
         },
         {
-            image: "./project-images/12.jpg",
-            name: "Food Junkies3",
-            description: "lorem"
+            image: "./project-images/placeholder.jpg",
+            name: "(Module 21 Assignment)",
+            description: "A xxxxxx site that was created using the MERN Stack & GraphQL",
+            appLink: "https://github.com/cmuscari",
+            githubLink: "https://github.com/cmuscari"
+        },
+        {
+            image: "./project-images/placeholder.jpg",
+            name: "(Group Project 3)",
+            description: "A xxxxxx site that was created using the MERN Stack & GraphQL",
+            appLink: "https://github.com/cmuscari",
+            githubLink: "https://github.com/cmuscari"
         }
     ]
 
@@ -25,7 +45,7 @@ function Portfolio() {
             <div className="projects-section">
                 <div className="projects-gallery">
                   {projectArray.map((project) => 
-                    <Project image={project.image} name={project.name} description={project.description} />
+                    <Project image={project.image} name={project.name} description={project.description} appLink={project.appLink} githubLink={project.githubLink} />
                   )}
                 </div>
             </div>
--- muscari-portfolio/src/components/Project/index.js
+++ muscari-portfolio/src/components/Project/index.js
@@ -5,6 +5,10 @@ function Project(props) {
         <div className="project-cell">
             <img src={props.image} alt="" className="project-cell-image" />
             <div className="project-cell-text">{props.name}</div>
+            <div className="project-cell-text">{props.description}</div>
+            <p className="project-cell-text">Check out the</p>
+            <a className="project-cell-text" href={props.appLink}>Deployed Application</a>
+            <a className="project-cell-text" href={props.githubLink}>GitHub Repository</a>
         </div>
     )
 };
