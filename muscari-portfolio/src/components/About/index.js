6e910c180df1bcc9dde4b619d90090d4fdcd2620
Cortnie <cortniemuscari@gmail.com>
2022-06-23 01:44:18 -0500

    about page styling

--- muscari-portfolio/src/components/About/index.js
+++ muscari-portfolio/src/components/About/index.js
@@ -4,15 +4,19 @@ import aboutImage from "../../assets/images/about/about-image.jpg";
 function About() {
 
     return (
-        <section className="my-5" >
-            <h1 className="about-header" id="about">Hi, I'm Cortnie!</h1>
-            <img src={aboutImage} className="my-2" style={{ width: "100%" }} alt="about image" />
-            <div className="my-2">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-                </p>
+        <div className="about-page d-flex flex-column justify-content-center align-items-center" id="about">
+            <div className="section-1 d-flex flex-row align-items-center">
+                <div className="section-1-text">
+                    <h1 className="section-1-title">Hi, I'm Cortnie!</h1>
+                    <p className="section-1-text-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. 
+                    </p>
+                </div>
+                <div>
+                    <img src={aboutImage} className="section-1-image" alt="about image" />
+                </div>
             </div>
-        </section>
+        </div>
     )
 }
 
