76faabf9a0f45841649e194179a2e478d66cd33e
Cortnie <cortniemuscari@gmail.com>
2022-06-23 12:59:16 -0500

    about section styling

--- muscari-portfolio/src/components/About/index.js
+++ muscari-portfolio/src/components/About/index.js
@@ -16,19 +16,19 @@ function About() {
                     <img src={aboutImage} className="section-1-image" alt="about image" />
                 </div>
             </div>
-            
+
             <div className="section-2 d-flex flex-row align-items-center">
+                <div>
+                    <img src={aboutImage} className="section-2-image" alt="my story image" />
+                </div>
                 <div className="section-2-text">
                     <h1 className="section-2-title">My Story</h1>
                     <p className="section-2-text-content">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
                     </p>
                 </div>
-                <div>
-                    <img src={aboutImage} className="section-2-image" alt="my story image" />
-                </div>
             </div>
-            
+
             <div className="section-3 d-flex flex-row align-items-center">
                 <div className="section-3-text">
                     <h1 className="section-3-title">My hobbies + interests</h1>
@@ -40,8 +40,11 @@ function About() {
                     <img src={aboutImage} className="section-3-image" alt="hobbies image" />
                 </div>
             </div>
-           
+
             <div className="section-4 d-flex flex-row align-items-center">
+                <div>
+                    <img src={aboutImage} className="section-4-image" alt="quotation marks image" />
+                </div>
                 <div className="section-4-text">
                     <h1 className="section-4-title">Favorite Quote</h1>
                     <p className="section-4-text-content">
