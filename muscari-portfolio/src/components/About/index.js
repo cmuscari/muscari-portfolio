036584a56f1d87fea21cee0342bf638ca91b7447
Cortnie <cortniemuscari@gmail.com>
2022-06-23 21:09:15 -0500

    about page styling

--- muscari-portfolio/src/components/About/index.js
+++ muscari-portfolio/src/components/About/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import aboutImage from "../../assets/images/about/about-image.jpg";
 import quoteLeftImage from "../../assets/images/about/left-quote_light.png";
 import quoteRightImage from "../../assets/images/about/right-quote_light.png";
+import photographyIcon from "../../assets/images/icons/photography.png";
 
 function About() {
 
@@ -31,15 +32,11 @@ function About() {
                 </div>
             </div>
 
-            <div className="section-3 d-flex flex-row align-items-center">
-                <div className="section-3-text">
-                    <h1 className="section-3-title">My hobbies + interests</h1>
-                    <p className="section-3-text-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
-                    </p>
-                </div>
-                <div>
-                    <img src={aboutImage} className="section-3-image" alt="hobbies image" />
+            <div className="section-3 d-flex flex-column align-items-center justify-content-center">
+                <h1 className="section-3-title">My hobbies + interests</h1>
+                <div className="d-flex flex-column align-items-center justify-content-center">
+                    <img src={photographyIcon} className="hobby-icon" alt="photography icon" />
+                    <p className="hobby-title">p h o t o g r a p h y</p>
                 </div>
             </div>
 
