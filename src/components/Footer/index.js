d40d682d912716e2b6f7a6fededbd58f6b6e966c
Cortnie <cortniemuscari@gmail.com>
2022-07-02 01:56:10 -0500

    responsive update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,13 +7,13 @@ function Footer() {
     return (
         <footer className="footer d-flex flex-row justify-content-center align-items-center">
             <a href="https://github.com/cmuscari" target="_blank">
-                <img src={githubImage} alt="Github Icon" className="icon m-2" />
+                <img src={githubImage} alt="Github Icon" className="icon" />
             </a>
             <a href="https://www.linkedin.com/in/cortnie-muscari/" target="_blank">
-                <img src={linkedinImage} alt="LinkedIn Icon" className="icon m-2" />
+                <img src={linkedinImage} alt="LinkedIn Icon" className="icon" />
             </a>
             <a href="https://leetcode.com/cmuscari/" target="_blank">
-                <img src={leetcodeImage} alt="LeetCode Icon" className="icon m-2" />
+                <img src={leetcodeImage} alt="LeetCode Icon" className="icon" />
             </a>
         </footer>
     );
